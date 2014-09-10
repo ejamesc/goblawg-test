a225a35f9bfd06fe792913c3b3b93f845b7ea13e
Cedric Chin <cedric@elijames.org>
2014-09-10 22:58:43 +0700

    Apply go fmt. Silly me.

--- blog.go
+++ blog.go
@@ -1,70 +1,70 @@
 package main
 
 import (
-  "io/ioutil"
-  "net/http"
-  "html/template"
-  
-  "github.com/gorilla/mux"
-  "github.com/russross/blackfriday"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/russross/blackfriday"
 )
 
 type Post struct {
-  Title string
-  Body []byte
+	Title string
+	Body  []byte
 }
 
 type Content struct {
-  Title template.HTML
-  Body template.HTML
+	Title template.HTML
+	Body  template.HTML
 }
 
 func (p *Post) save() error {
-  filename := p.Title + ".md"
-  return ioutil.WriteFile(filename, p.Body, 0600)
+	filename := p.Title + ".md"
+	return ioutil.WriteFile(filename, p.Body, 0600)
 }
 
 func loadPost(title string) (*Content, error) {
-  filename := title + ".md"
-  body, err := ioutil.ReadFile(filename)
-  if err != nil {
-    return nil, err
-  }
+	filename := title + ".md"
+	body, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
 
-  htmlBody := blackfriday.MarkdownCommon(body)
-  return &Content{Title: template.HTML(title), Body: template.HTML(htmlBody)}, nil
+	htmlBody := blackfriday.MarkdownCommon(body)
+	return &Content{Title: template.HTML(title), Body: template.HTML(htmlBody)}, nil
 }
 
-// TODO: better way to do this?  
+// TODO: better way to do this?
 var templates = template.Must(template.ParseFiles(
-    "templates/partials.html",
-    "templates/admin.html",
-    "templates/view.html"))
+	"templates/partials.html",
+	"templates/admin.html",
+	"templates/view.html"))
 
 func main() {
-  r := mux.NewRouter()
-  r.HandleFunc("/admin/", adminHandler);
-  r.HandleFunc("/view/{title}", viewHandler);
+	r := mux.NewRouter()
+	r.HandleFunc("/admin/", adminHandler)
+	r.HandleFunc("/view/{title}", viewHandler)
 
-  http.Handle("/", r)
-  http.ListenAndServe(":8080", nil)
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", nil)
 }
 
 /* HANDLERS */
 // TODO: List out all posts
 func adminHandler(writer http.ResponseWriter, request *http.Request) {
-   renderTemplate(writer, "admin", nil)
+	renderTemplate(writer, "admin", nil)
 }
 
 func viewHandler(writer http.ResponseWriter, request *http.Request) {
-  vars := mux.Vars(request)
-  post, err := loadPost(vars["title"])
-  if (err != nil) {
-    http.Error(writer, err.Error(), http.StatusInternalServerError)
-    return
-  }
+	vars := mux.Vars(request)
+	post, err := loadPost(vars["title"])
+	if err != nil {
+		http.Error(writer, err.Error(), http.StatusInternalServerError)
+		return
+	}
 
-  renderTemplate(writer, "view", post)
+	renderTemplate(writer, "view", post)
 }
 
 func renderTemplate(w http.ResponseWriter, tmpl string, c *Content) {
